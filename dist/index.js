a4ef9f618e1c433909226ac147e8949b68965db7
Rony Garcia <ronygcgarcia@gmail.com>
2022-07-22 19:31:47 -0600

    feat: auth module migration with relationships

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./configs/Env");
-const main_1 = __importDefault(require("./main"));
-new main_1.default();
