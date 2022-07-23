1925c268774dffc431d6cb2b9b3282740df9a345
Rony Garcia <ronygcgarcia@gmail.com>
2022-07-23 12:02:39 -0600

    fix: clear api routes file and ApiController

--- app/controllers/ApiController.ts
+++ app/controllers/ApiController.ts
@@ -8,97 +8,74 @@ import Auth from "../utils/Auth";
 const prisma = new PrismaClient();
 
 export default class ApiController {
-  static async login(req: Request, res: Response) {
-    const { email, password }: { email: string; password: string } = req.body;
+    static async login(req: Request, res: Response) {
+        const { email, password }: { email: string; password: string } = req.body;
 
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) throw new NoAuthException("Invalid credentials");
-    if (user.is_suspended) throw new NoAuthException("The user is suspended");
-
-    const validPassword = bcrypt.compareSync(password, user.password);
-    if (!validPassword) throw new NoAuthException("Invalid credentials");
-
-    await prisma.user.update({
-      where: {
-        id: user.id,
-      },
-      data: {
-        last_login: moment().format(),
-      },
-    });
-
-    const userInfo = {
-      id: user.id,
-      email: user.email,
-      last_login: user.last_login,
-    };
-
-    const userProfiles = await prisma.user.findUnique({
-      where: {
-        id: user.id,
-      },
-      include: {
-        UserProfile: {
-          include: {
-            profile: {
-              include: {
-                ProfileRole: {
-                  include: {
-                    role: true,
-                  },
-                },
-              },
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
             },
-          },
-        },
-      },
-    });
+        });
 
-    const roles = userProfiles?.UserProfile.map((userProfile) => {
-      return userProfile.profile?.ProfileRole.map((profileRole) => {
-        return profileRole.role.name;
-      });
-    });
+        if (!user) throw new NoAuthException("Invalid credentials");
+        if (user.is_suspended) throw new NoAuthException("The user is suspended");
 
-    const token = {
-      user: userInfo,
-      roles: roles?.flat(2),
-    };
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (!validPassword) throw new NoAuthException("Invalid credentials");
 
-    const secretKey: string = process.env.SECRET_KEY || "";
-    const response = {
-      token: await Auth.createToken(token, secretKey),
-      refresh_token: await Auth.refreshToken(user),
-    };
+        await prisma.user.update({
+            where: {
+                id: user.id,
+            },
+            data: {
+                last_login: moment().format(),
+            },
+        });
 
-    return res.status(HttpCode.HTTP_OK).json(response);
-  }
+        const userInfo = {
+            id: user.id,
+            email: user.email,
+            last_login: user.last_login,
+        };
 
-  static async logout(req: Request, res: Response) {}
-  static async signin(req: Request, res: Response) {}
-  static async createProduct(req: Request, res: Response) {}
-  static async getProducts(req: Request, res: Response) {
-    const { product } = prisma;
-    const productList = await product.findMany();
-    return res.status(HttpCode.HTTP_OK).json(productList);
-  }
-  static async getProduct(req: Request, res: Response) {}
-  static async updateProduct(req: Request, res: Response) {}
-  static async deleteProduct(req: Request, res: Response) {}
-  static async disableProduct(req: Request, res: Response) {}
-  static async reactToProduct(req: Request, res: Response) {}
-  static async getAllOrders(req: Request, res: Response) {
-    const { order } = prisma;
-    const orderList = await order.findMany();
-    return res.status(HttpCode.HTTP_OK).json(orderList);
-  }
-  static async getOrder(req: Request, res: Response) {}
-  static async addToCart(req: Request, res: Response) {}
-  static async getCart(req: Request, res: Response) {}
-  static async buyOrder(req: Request, res: Response) {}
+        const userProfiles = await prisma.user.findUnique({
+            where: {
+                id: user.id,
+            },
+            include: {
+                UserProfile: {
+                    include: {
+                        profile: {
+                            include: {
+                                ProfileRole: {
+                                    include: {
+                                        role: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        });
+
+        const roles = userProfiles?.UserProfile.map((userProfile) => {
+            return userProfile.profile?.ProfileRole.map((profileRole) => {
+                return profileRole.role.name;
+            });
+        });
+
+        const token = {
+            user: userInfo,
+            roles: roles?.flat(2),
+        };
+
+        const secretKey: string = process.env.SECRET_KEY || "";
+        const response = {
+            token: await Auth.createToken(token, secretKey),
+            refresh_token: await Auth.refreshToken(user),
+        };
+
+        return res.status(HttpCode.HTTP_OK).json(response);
+    }
 }
