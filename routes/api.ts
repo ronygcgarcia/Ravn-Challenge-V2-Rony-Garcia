3a3dff583c5cb81bb54e843e4c7b7b30c0c14090
Rony Garcia <ronygcgarcia@gmail.com>
2022-07-23 23:11:42 -0600

    feat: adds product list endpoint with pagination and category filter

--- routes/api.ts
+++ routes/api.ts
@@ -5,6 +5,7 @@ import auth from '../app/middlewares/Auth';
 import validation from '../app/middlewares/validate';
 import loginSchema from '../app/schemas/loginSchema';
 import signupSchema from '../app/schemas/signupSchema';
+import productRoutes from './api/productRoutes';
 
 
 const router = Router();
@@ -12,5 +13,7 @@ router.post('/v1/login', [validation(loginSchema)], Call(ApiController.login));
 router.post('/v1/logout', [auth], Call(ApiController.logout));
 router.post('/v1/signup', [validation(signupSchema)], Call(ApiController.signup));
 router.get('/v1/user/confirm/:token', Call(ApiController.confirmUser));
+
+router.use('/v1/products', productRoutes);
 
 export default router;
