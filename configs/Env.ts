ba4c38a4cd8a2234965cf4e82fc7602d72d460fd
Rony Garcia <ronygcgarcia@gmail.com>
2022-07-26 16:47:58 -0600

    fix: dotenv config file

--- configs/Env.ts
+++ configs/Env.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 
 import path from 'path';
 
-const envFile = process.env.APP_ENV ? `.env.${process.env.APP_ENV}` : '.env';
 dotenv.config({
-  path: path.resolve(envFile),
+  path: path.resolve('.env'),
 });
