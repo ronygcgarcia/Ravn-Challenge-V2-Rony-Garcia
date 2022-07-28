115d06ec0360d44856d74eba9a0618e10d1264ea
Rony Garcia <ronygcgarcia@gmail.com>
2022-07-28 11:05:32 -0600

    fix: changes seeder information for categories and products

--- prisma/categories.ts
+++ prisma/categories.ts
@@ -1,19 +1,47 @@
 const categories = [
     {
         id: 1,
-        name: 'Category 1'
+        name: 'Cookies & Baked Goods'
     },
     {
         id: 2,
-        name: 'Category 2'
+        name: 'Jerky'
     },
     {
         id: 3,
-        name: 'Category 3'
+        name: 'Dehydrated fruit'
     },
     {
         id: 4,
-        name: 'Category 4'
+        name: 'Popcorn'
+    },
+    {
+        id: 5,
+        name: 'Granola'
+    },
+    {
+        id: 6,
+        name: 'Chips'
+    },
+    {
+        id: 7,
+        name: 'Crackers & Crisps'
+    },
+    {
+        id: 8,
+        name: 'Bars & Balls'
+    },
+    {
+        id: 9,
+        name: 'Hard Pretzels'
+    },
+    {
+        id: 10,
+        name: 'Snack Mixes'
+    },
+    {
+        id: 11,
+        name: 'Puffs'
     }
 ];
 
